1ec4e43a61f1b633cac989260b95281981229d0e
Kevin W <kevixw@gmail.com>
2025-11-16 21:30:44 +0000

    on the session list page, you should have a column "Job Name", which dis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
 
   if (!isClient) {
     return (
-      <div className="flex flex-col min-h-screen bg-background">
+      <div className="flex flex-col flex-1 bg-background">
         <main className="flex-1 overflow-auto p-4 sm:p-6 md:p-8">
           <div className="container mx-auto max-w-4xl space-y-8">
              <div className="space-y-8">
