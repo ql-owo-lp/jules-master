1e50c61382e3dad42f0a34c4d8c2f52c38e51615
Kevin W <kevixw@gmail.com>
2025-11-17 05:01:36 +0000

    make the whole page adapt to the width of user's browser width

--- src/app/jobs/new/page.tsx
+++ src/app/jobs/new/page.tsx
@@ -74,7 +74,7 @@ export default function NewJobPage() {
   return (
     <div className="flex flex-col flex-1 bg-background">
       <main className="flex-1 overflow-auto p-4 sm:p-6 md:p-8">
-        <div className="container mx-auto max-w-4xl space-y-8">
+        <div className="space-y-8 px-4 sm:px-6 lg:px-8">
           {!isClient ? (
              <div className="space-y-8">
               <Skeleton className="h-12 w-full" />
