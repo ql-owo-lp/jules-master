4cee5d949b64ab430373e75d5068b40269265381
ql-owo-lp <kevixw@gmail.com>
2025-11-20 21:42:23 -0800

    Revert "Add Playwright E2E tests"
    This reverts commit a3effcb8f73d82d22f3484b582c4828f19bebce5.

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   test: {
     globals: true,
     setupFiles: ['./tests/setup.ts'],
-    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', 'tests/e2e/**'],
   },
   resolve: {
     alias: {
